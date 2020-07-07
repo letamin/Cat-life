7071a4bce66d4b31709dec2526c27f07330bc130
letamin <lethanhminh0901@gmail.com>
2020-07-08 05:41:32 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+const introContainer = [...document.querySelectorAll('.intro-container')];
+const dots = [...document.querySelectorAll('.intro-links')];
+const burgerButton = document.querySelector('.burger');
+const navbarUL = document.querySelector('.navbar-ul');
+
+var slideIndex = 1;
+
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready);
+} else {
+    ready();
+}
+
+function ready() {
+    showSlides(slideIndex);
+    burgerActive();
+    setInterval(() => {
+        showSlides(++slideIndex);
+    }, 5000);
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    if (n > introContainer.length) slideIndex = 1;
+    if (n < 1) slideIndex = introContainer.length;
+    for (let i = 0; i < introContainer.length; i++) {
+        introContainer[i].classList.remove('visible');
+        dots[i].classList.remove('here');
+    }
+
+    introContainer[slideIndex - 1].style.setProperty('--img', `url(/images/${slideIndex}.jpg)`);
+    introContainer[slideIndex - 1].classList.add('visible');
+    dots[slideIndex - 1].classList.add('here');
+}
+
+function burgerActive() {
+    burgerButton.addEventListener('click', () => {
+        navbarUL.classList.toggle('burger-active');
+    })
+}
+
